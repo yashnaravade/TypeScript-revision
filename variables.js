8f043639938c18e3c6a0e011265534a5be1410ee
Yash Naravade <yashdip123@gmail.com>
2022-12-19 12:26:11 +0530

    Variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var userID = 69;
+var userName = "John Doe";
+var isUserLoggedIn = true;
+// used `tsc variables.ts` to compile the file to JS
+// used `ts-node variables.ts` to run the file
+// typescript automatically infers the type of the variable if it is not explicitly defined.
+userID.toFixed(2); // 69.00
+console.log(userName.toUpperCase()); // JOHN DOE
+console.log(userID.toFixed(2));
---
+++ variables.ts
@@ -0,0 +1,17 @@
+let userID =69;
+
+let userName: string = "John Doe";
+
+let isUserLoggedIn: boolean = true;
+
+// used `tsc variables.ts` to compile the file to JS
+// used `ts-node variables.ts` to run the file
+// typescript automatically infers the type of the variable if it is not explicitly defined.
+
+userID.toFixed(2); // 69.00
+
+console.log(userName.toUpperCase()); // JOHN DOE
+console.log(userID.toFixed(2));
+
+export {};
+
