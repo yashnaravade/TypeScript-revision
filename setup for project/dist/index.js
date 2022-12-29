10f94b300e0dcad26841a55838272b7b091054c8
Yash Naravade <yashdip123@gmail.com>
2022-12-29 16:30:45 +0530

    Classes

--- setup for project/dist/index.js
+++ setup for project/dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("Hello World!");
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const user = new User("John", 30);
+user.age = 21; // change age
+console.log(user.name); // John
+console.log(user.age); // 21
--- setup for project/src/index.ts
+++ setup for project/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+class User {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const user = new User("John", 30);
+
+user.age= 21; // change age
+
+console.log(user.name); // John
+console.log(user.age); // 21
+
