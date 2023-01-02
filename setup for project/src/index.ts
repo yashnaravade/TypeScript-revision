9aaabf5d320795180b78daa7f74a04c286a6e282
Yash Naravade <yashdip123@gmail.com>
2023-01-02 10:11:42 +0530

    Public private

--- setup for project/src/index.ts
+++ setup for project/src/index.ts
@@ -1,16 +1,28 @@
+// class User {
+//   name: string;
+//   age: number;
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// const user = new User("John", 30);
+
+// user.age = 21; // change age
+
+// console.log(user.name); // John
+// console.log(user.age); // 21
+
 class User {
-  name: string;
-  age: number;
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
+  constructor(
+    public name: string, 
+    public age: number,
+    private userId= 123
+    ) {
   }
 }
 
-const user = new User("John", 30);
+const Yash = new User("Yash", 21); // 123 is the default value for userId as it is not passed in the constructor call. 
 
-user.age= 21; // change age
-
-console.log(user.name); // John
-console.log(user.age); // 21
-
+console.log(Yash.name); // Yash
